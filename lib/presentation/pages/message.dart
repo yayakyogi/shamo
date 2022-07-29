e86390ce78f18b4252d6349919dadccf1f7973b1
yayakyogi <yayaktaka@gmail.com>
2022-07-29 09:36:39 +0700

    add widget list message

--- lib/presentation/pages/message.dart
+++ lib/presentation/pages/message.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shamo/core/utils/transition_page.dart';
+import 'package:shamo/presentation/pages/message_content.dart';
 import 'package:shamo/presentation/themes/themes.dart';
 import 'package:shamo/presentation/widgets/button_primary.dart';
 
@@ -47,6 +49,79 @@ class _MessageState extends State<Message> {
       );
     }
 
+    // widget card message
+    Widget cardMessage() {
+      return GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            EnterPage(page: MessageContent()),
+          );
+        },
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 30),
+          padding: const EdgeInsets.only(bottom: 12),
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: bgColor2,
+              ),
+            ),
+          ),
+          child: Row(
+            children: [
+              Image.asset(
+                'assets/icons/ic_image_shop_logo.png',
+                width: 54,
+                height: 54,
+              ),
+              const SizedBox(width: 12),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Shoe Store',
+                      style: fw_400.copyWith(
+                        fontSize: 15,
+                        color: primaryTextColor,
+                      ),
+                    ),
+                    Text(
+                      'Good night, This item is on asjdksdh nsdj',
+                      style: fw_400.copyWith(
+                        fontSize: 15,
+                        color: secondaryTextColor,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'Now',
+                style: fw_400.copyWith(
+                  fontSize: 10,
+                  color: secondaryTextColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    // widget list message
+    Widget listMessage() {
+      return ListView(
+        children: [
+          const SizedBox(height: 33),
+          cardMessage(),
+        ],
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 87,
@@ -62,7 +137,7 @@ class _MessageState extends State<Message> {
       ),
       body: SizedBox(
         height: MediaQuery.of(context).size.height,
-        child: nullMessage(),
+        child: listMessage(),
       ),
     );
   }
