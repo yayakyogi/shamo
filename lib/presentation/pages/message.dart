21c4a924dacb4328e0cde82f5895c3e778ab03ae
yayakyogi <yayaktaka@gmail.com>
2022-07-30 23:29:24 +0700

    add widget empty content

--- lib/presentation/pages/message.dart
+++ lib/presentation/pages/message.dart
@@ -3,6 +3,7 @@ import 'package:shamo/core/utils/transition_page.dart';
 import 'package:shamo/presentation/pages/message_content.dart';
 import 'package:shamo/presentation/themes/themes.dart';
 import 'package:shamo/presentation/widgets/button_primary.dart';
+import 'package:shamo/presentation/widgets/empty_content.dart';
 
 class Message extends StatefulWidget {
   const Message({Key? key}) : super(key: key);
@@ -14,41 +15,6 @@ class Message extends StatefulWidget {
 class _MessageState extends State<Message> {
   @override
   Widget build(BuildContext context) {
-    // widget jika pesan kosong
-    Widget nullMessage() {
-      return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/icons/ic_headset.png',
-              width: 80,
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Opss no message yet?',
-              style: fw_500.copyWith(
-                fontSize: 16,
-                color: primaryTextColor,
-              ),
-            ),
-            const SizedBox(height: 12),
-            Text(
-              'You have never done a transaction',
-              style: fw_500.copyWith(
-                color: secondaryTextColor,
-              ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: 152,
-              child: ButtonPrimary(onPressed: () {}, title: 'Explore Store'),
-            )
-          ],
-        ),
-      );
-    }
-
     // widget card message
     Widget cardMessage() {
       return GestureDetector(
@@ -137,7 +103,12 @@ class _MessageState extends State<Message> {
       ),
       body: SizedBox(
         height: MediaQuery.of(context).size.height,
-        child: listMessage(),
+        child: EmptyContent(
+          icon: 'assets/icons/ic_headset.png',
+          messageTitle: 'Opss no message yet?',
+          messageSubTitle: 'You have never done a transaction',
+          onPressed: () {},
+        ),
       ),
     );
   }
